d47680ce92b33c9a34926bda6a77809ac8619f55
Fabio de Araujo <fabioaraujo1608@gmail.com>
2025-12-09 22:36:19 -0300

    Implementando MQTT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,6 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.sounddetection"
+
     compileSdk {
         version = release(36)
     }
@@ -33,6 +36,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    fun Packaging.() {
+        resources {
+            excludes += listOf("META-INF/INDEX.LIST", "META-INF/io.netty.versions.properties")
+        }
+    }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -50,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.10")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
